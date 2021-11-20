6825c05eb494af040aa3e3368e76f475e103a45f
류재성 <rjs4902@naver.com>
2021-11-20 23:29:08 +0900

    2021.11.20 - 초기 코드 짜기

--- venv/Include/Main.py
+++ venv/Include/Main.py
@@ -5,18 +5,28 @@
 """
 
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QToolTip
-from PyQt5.QtGui import QIcon
-from PyQt5.QtCore import QCoreApplication
-from PyQt5.QtGui import QFont
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
 
-class MainApp(QWidget):
+class MainApp:
 
     def __init__(self):
         super().__init__()
+        self.date = QDateTime.currentDateTime()
         self.initUI()
 
     def initUI(self):
+        exitAction = QtWidgets.QAction(QtGui.QIcon('../resources/icon/exit.png'), 'Exit', self)
+        exitAction.setShortcut('Ctrl+Q')
+        exitAction.setStatusTip('Exit application')
+        exitAction.triggered.connect(qApp.quit)
+
+        menubar = QMainWindow.menuBar()
+        menubar.setNativeMenuBar(False)
+        filemenu = menubar.addMenu('&File')
+        filemenu.addAction(exitAction)
+
         btn = QPushButton('Build', self)
         btn.move(100, 400)
         btn.resize(btn.sizeHint())
@@ -25,17 +35,34 @@ class MainApp(QWidget):
         btn.setToolTip('*.cer, *.der, *.crt 파일만 첨부가 가능합니다.')
 
         btn1= QPushButton('Exit', self)
-        btn1.move(325, 400)
+        btn1.move(315, 400)
         btn1.resize(btn.sizeHint())
         btn1.clicked.connect(QCoreApplication.instance().quit)
 
         QToolTip.setFont(QFont('SansNoto', 10))
         btn1.setToolTip('실행 창을 종료합니다.')
 
+        # self.date.toString(Qt.DefaultLocaleLongDate)
+        MainstatusBar = QStatusBar()
+        self.setStatusBar(MainstatusBar)
+
+        status1 = QProgressBar()
+        status1.setValue(24)
+        MainstatusBar.addPermanentWidget(status1)
+
+        self.statusBar().showMessage('Ready')
+
         self.setWindowTitle('Cert Package Build')
         self.setWindowIcon(QIcon('../resources/icon/icon.png'))
-        self.setGeometry(300, 300, 500, 500)
+        self.resize(500, 500)
+        self.center()
         self.show()
+
+    def center(self):
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
 
 
 if __name__ == '__main__':
